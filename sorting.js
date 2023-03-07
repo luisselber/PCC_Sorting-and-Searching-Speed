96e77c959f58f65abc69ca40cbcdce422e0071fe
pedrodcarvalho <pedrodc51203@gmail.com>
2023-03-07 15:57:55 -0300

    Merge Sort Implemented

--- sorting.js
+++ sorting.js
@@ -1,4 +1,4 @@
-//Bubble
+// Bubble
 function bubbleSort(unsortedArray) {
     let flag = false;
 
@@ -20,4 +20,32 @@ function bubbleSort(unsortedArray) {
             return;
         }
     }
+}
+
+// Merge
+function mergeSort(unsortedArray) {
+    const middleIndex = unsortedArray.length / 2;
+
+    if (unsortedArray.length < 2) {
+        return unsortedArray;
+    }
+
+    const leftSubArray = unsortedArray.splice(0, middleIndex);
+
+    return mergeSubArrays(mergeSort(leftSubArray), mergeSort(unsortedArray));
+}
+
+function mergeSubArrays(leftSubArray, rightSubArray) {
+    let array = [];
+
+    while (leftSubArray.length && rightSubArray.length) {
+        if (leftSubArray[0] < rightSubArray[0]) {
+            array.push(leftSubArray.shift());
+        }
+        else {
+            array.push(rightSubArray.shift());
+        }
+    }
+
+    return [...array, ...leftSubArray, ...rightSubArray];
 }
